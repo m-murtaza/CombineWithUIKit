c75f9c96433772f453dc2d13ebe88eb1e0e92104
Mashood Murtaza <mashood.murtaza@gmail.com>
2025-08-02 01:32:15 +0500

    added movie list view model, call HTTPClient call with combine, used debounce

---
+++ CombineWithUIKit/ViewModels/MovieListViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  MovieListViewModel.swift
+//  CombineWithUIKit
+//
+//  Created by Mashud Murtaza on 02/08/2025.
+//
+
+import Foundation
+import Combine
+
+class MovieListViewModel {
+    
+    @Published private(set) var movies: [Movie] = []
+    private var cancellables: Set<AnyCancellable> = []
+    @Published var loadingCompleted: Bool = false
+    
+    private var searchSubject = CurrentValueSubject<String, Never>("")
+    
+    private let httpClient: HTTPClient
+    
+    init(httpClient: HTTPClient) {
+        self.httpClient = httpClient
+        setupSearchPublisher()
+    }
+    
+    private func setupSearchPublisher() {
+        
+        searchSubject
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .sink { [weak self] searchText in
+                self?.loadMovies(search: searchText)
+            }.store(in: &cancellables)
+        
+    }
+    
+    func setSearchText(_ searchText: String) {
+        searchSubject.send(searchText)
+    }
+    
+    func loadMovies(search: String) {
+        
+        httpClient.fetchMovies(search: search)
+            .sink { [weak self] completion in
+                switch completion {
+                    case .finished:
+                        self?.loadingCompleted = true
+                    case .failure(let error):
+                        print(error)
+                }
+            } receiveValue: { [weak self] movies in
+                self?.movies = movies
+            }
+            .store(in: &cancellables)
+    }
+}
